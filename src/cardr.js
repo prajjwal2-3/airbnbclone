76803697847034433ce47091671fb782afbf4496
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2023-10-20 19:31:13 +0530

    okm

--- src/cardr.js
+++ src/cardr.js
@@ -1,37 +1,24 @@
 import Card from './card.js';
-import image1 from './image 12.png';
-import image2 from './image2.png';
-import image3 from './image3.png';
+
+import data from './data.js'
 import './card.css'
+
+
+const cards = data.map(item => (
+  <Card
+    image={item.coverImg}
+    para={item.title}
+    star={item.stats.rating}
+    no={item.stats.reviewCount}
+    country={item.location}
+    price={item.price}
+  />
+));
+
 
 function Crd(){
     return(
-        <div className='forrow'>
-            <Card
-      image = {image1}
-      para = "Life Lessons with katie Zaferes"
-      star = "5.0"
-      no = "6"
-      country = "USA"
-      price = "$136"
-    />
-    <Card
-      image = {image2}
-      para = "Group Mountain Biking"
-      star = "4.8"
-      no = "2"
-      country = "USA"
-      price = "$50"
-    />
-    <Card
-      image = {image3}
-      para = "Learn wedding photography"
-      star = "5.0"
-      no = "30"
-      country = "USA"
-      price = "$125"
-    />
-        </div>
+      cards
     )
 }
 
