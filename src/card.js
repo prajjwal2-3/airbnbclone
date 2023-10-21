2a18433511e4bb51deaa76f06b7fed94f7e6b984
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2023-10-21 23:35:20 +0530

    uyfuy

--- src/card.js
+++ src/card.js
@@ -5,15 +5,24 @@ import './card.css'
 
 
 function Card(props){
+  let badge
+  if (props.openspots===0){
+  badge = "SOLD OUT"
+  }else if (props.country==="Online"){
+    badge = "Online"
+  }
+
     return(
         
         <div className='card'>
+          {badge && <div className='card--badge'>{badge}</div>}
     <img src={props.image} alt="nothing to show " className='image'></img>
     <span className="">{props.star}</span>
     <span className=""> ({props.no})</span>
     <span className="">.{props.country}</span>
     <p className="para">{props.para}</p>
     <h3 className ="price">From {props.price}/ person</h3>
+
   </div>
         
     )
@@ -23,12 +32,13 @@ function Card(props){
 
 const cards = data.map(item => (
   <Card
-    image={item.coverImg}
+   image={item.coverImg}
     para={item.title}
     star={item.stats.rating}
     no={item.stats.reviewCount}
     country={item.location}
     price={item.price}
+    openspots = {item.openSpots}
   />
 ));
 
